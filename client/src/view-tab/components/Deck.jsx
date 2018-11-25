9c3306f5650df19396775b761921ff25b04dba5d
Javi Garcia-Bengochea <jgb4410@gmail.com>
2018-11-25 01:12:07 -0800

    Added functionailty for editing, fixed deletings

--- client/src/view-tab/components/Deck.jsx
+++ client/src/view-tab/components/Deck.jsx
@@ -2,42 +2,50 @@ import React from 'react';
 import axios from 'axios';
 import { LOCAL_IP } from './../../../../config.js';
 import CardDrawer from './CardDrawer.jsx';
+import DeckForm from './DeckForm.jsx';
 
 export default class Deck extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      expanded: false
+      expanded: false,
+      editing: false
     }
+    this.handleDeleteDeck = this.handleDeleteDeck.bind(this);
     this.expandDeck = this.expandDeck.bind(this);
+    this.editDeck = this.editDeck.bind(this);
   }
 
-  handleDeleteDeck(name) {
-    axios.delete(`http://${LOCAL_IP}:3000/momento/deck`, { data: { name }})
-      .then(() => {
-        this.props.setDecks();
-      })
-      .catch(err => console.error(err));
+  handleDeleteDeck(e, name = this.props.deck.name) {
+    e.preventDefault();
+    axios.delete(`http://${LOCAL_IP}:3000/momento/deck`, { data: { name } })
+      .then(this.props.setDecks)
+      .catch(console.error);
   }
 
   expandDeck() {
-    this.setState({ expanded: !this.state.expanded})
+    this.setState({ expanded: !this.state.expanded })
+  }
+  
+  editDeck(deck = this.props.deck) {
+    this.setState({ editing: !this.state.editing })
   }
 
   render() {
     return (
       <div className='deck-container'>
         <div className='deck'>
-          <div className='deck-name' onClick={this.expandDeck}>{this.props.deck.name}</div>
-          <div className='deck-description'>{this.props.deck.description}</div>
+          <div className='deck-name' onClick={ this.expandDeck } >{ this.props.deck.name }</div>
+          <div className='deck-description' >{ this.props.deck.description }</div>
           <div>
-            <div>edit</div>
-            <div onClick={() => this.handleDeleteDeck(this.props.deck.name)}>
+            <div onClick={ this.editDeck } >edit</div>
+            <div onClick={ this.handleDeleteDeck } >
               <img src='../lib/assets/trash.png' alt='settings icon'/>
             </div>
           </div>
         </div>
-        {this.state.expanded ? <CardDrawer cards={this.props.deck.cards}/> : null}
+        {this.state.editing ? <DeckForm setDecks={ this.props.setDecks } oldDeck={ this.props.deck } /> : null}
+        {this.state.expanded ? <CardDrawer cards={ this.props.deck.cards }/> : null}
       </div>
     );
   }
