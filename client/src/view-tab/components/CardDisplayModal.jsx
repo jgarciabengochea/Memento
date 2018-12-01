813c8d84aee46320b46a4de00fedfa405116b4c9
Javi Garcia-Bengochea <jgb4410@gmail.com>
2018-11-30 21:44:25 -0800

    created barebones modal to show card in quiz display

---
+++ client/src/view-tab/components/CardDisplayModal.jsx
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+export default class CardDisplayModal extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+
+  render() {
+    let modalStyle = {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      zIndex: '9999',
+      background: '#fff'
+    };
+
+    let backdropStyle = {
+      position: 'absolute',
+      width: '100%',
+      height: '100%',
+      top: '0px',
+      left: '0px',
+      zIndex: '9998',
+      background: 'rgba(0, 0, 0, 0.3)'
+    };
+    return (
+      <div>
+        <div style={modalStyle}>
+          {this.props.card.front}
+        </div>
+        <div style={backdropStyle}></div>
+      </div>
+    )
+  }
+}
