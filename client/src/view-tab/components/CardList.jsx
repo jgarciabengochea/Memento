75da4e8f11c192b8a79298f1ac7a8121ab38346d
Javi Garcia-Bengochea <jgb4410@gmail.com>
2018-12-04 23:44:13 -0800

    refactored CardList to be stateless functional component

--- client/src/view-tab/components/CardList.jsx
+++ client/src/view-tab/components/CardList.jsx
@@ -1,19 +1,12 @@
 import React, { Component } from 'react';
 import CardListEntry from './CardListEntry.jsx';
 
-export default class CardList extends Component {
-  constructor(props) {
-    super(props);
+const CardList = ({ cards }) => (
+  <div className='card-drawer'>
+    {cards.map((card, i) => {
+      return <CardListEntry key={i} card={card}/>
+    })}
+  </div>
+);
 
-  }
-
-  render() {
-    return (
-      <div className='card-drawer'>
-        {this.props.cards.map((card, i) => {
-          return <CardListEntry key={i} card={card}/>
-        })}
-      </div>
-    );
-  }
-}
+export default CardList;
