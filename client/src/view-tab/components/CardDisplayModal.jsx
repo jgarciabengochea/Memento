d94fadd323187ce75a376ebe6ae37d305e8209f0
Javi Garcia-Bengochea <jgb4410@gmail.com>
2018-12-06 21:52:24 -0800

    moved modal styles from component to stylesheet

--- client/src/view-tab/components/CardDisplayModal.jsx
+++ client/src/view-tab/components/CardDisplayModal.jsx
@@ -7,27 +7,9 @@ export default class CardDisplayModal extends Component {
   }
 
   render() {
-    let modalStyle = {
-      position: 'absolute',
-      top: '50%',
-      left: '50%',
-      transform: 'translate(-50%, -50%)',
-      zIndex: '9999',
-      background: '#fff'
-    };
-
-    let backdropStyle = {
-      position: 'absolute',
-      width: '100%',
-      height: '100%',
-      top: '0px',
-      left: '0px',
-      zIndex: '9998',
-      background: 'rgba(0, 0, 0, 0.3)'
-    };
     return (
       <div>
-        <div style={modalStyle}>
+        <div className='modal'>
           <div className='card-front'>
             {this.props.card.front}
           </div>
@@ -35,7 +17,7 @@ export default class CardDisplayModal extends Component {
             {this.props.card.back}
           </div>
         </div>
-        <div style={backdropStyle}></div>
+        <div className='modal-backdrop'></div>
       </div>
     )
   }
