4b78d3d5087b8ddf9dce1940f49bff67a396a5c1
Javi Garcia-Bengochea <jgb4410@gmail.com>
2018-12-09 21:36:09 -0800

    Added dateCreated with moment

--- client/src/Models/CardModel.jsx
+++ client/src/Models/CardModel.jsx
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 export default class CardModel {
   constructor(front, back, index) {
     this.front = front;
@@ -6,5 +8,6 @@ export default class CardModel {
     this.bin = 0;
     this.correct = 0;
     this.incorrect = 0;
+    this.dateCreated = moment().format('MMMM Do YYYY');
   }
 }
