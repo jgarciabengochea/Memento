cafb7d2e5f8d754f15b77408fb9b133709d6b8a0
Javi Garcia-Bengochea <jgb4410@gmail.com>
2018-12-09 21:36:29 -0800

    added dateCreated with moment

--- client/src/Models/DeckModel.jsx
+++ client/src/Models/DeckModel.jsx
@@ -1,7 +1,10 @@
+import moment from 'moment';
+
 export default class DeckModel {
   constructor(name, description) {
     this.name = name;
     this.description = description;
     this.cards = [];
+    this.dateCreated = moment().format('MMMM Do YYYY');
   }
 }
