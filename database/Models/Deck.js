cf6d88c64716a3ff945cfa2cbea0394f23fa6787
Javi Garcia-Bengochea <jgb4410@gmail.com>
2018-12-11 13:04:11 -0800

    updated card and deck schemas to include new props

--- database/Models/Deck.js
+++ database/Models/Deck.js
@@ -1,22 +1,27 @@
 const mongoose = require('mongoose');
 // TODO: add support for new deck and card props
 const cardSchema = new mongoose.Schema({
+  deckId: String,
+  id: String,
   front: String,
   back: String,
   index: Number,
   bin: Number,
   correct: Number,
   incorrect: Number,
+  dateCreated: String
 });
 
 const deckSchema = new mongoose.Schema({
+  id: String,
   name: {
     type: String,
     index: true,
     unique: true
   },
   description: String,
-  cards: [cardSchema]
+  cards: [cardSchema],
+  dateCreated: String
 });
 
 const Deck = mongoose.model('Deck', deckSchema);
