7cfbcb2da902216bd779892d2f34f8811d1bf8bb
Javi Garcia-Bengochea <jgb4410@gmail.com>
2018-12-16 22:04:19 -0800

    implemented handlers for login/signup

--- server/controllers.js
+++ server/controllers.js
@@ -1,11 +1,33 @@
 const { saveDeck, findOneDeck, findAllDecks, updateDeck, updateDeckMetaData, deleteDeck } = require('../database/dbHelpers.js');
+const User = require('./../database/schemas/User.js');
+const Session = require('./../database/schemas/Session.js');
+const util = require('./lib/hashUtils.js');
+
+const isLoggedIn = (req, res) => {
+  Session.findOne({ hash: req.cookies.authToken })
+    .exec()
+    .then(session => {
+      if (!session) {
+        throw new Error('No session exists for this token');
+      }
+      return session.hash === req.cookies.authToken;
+    })
+    .then(isValid => {
+      if (!isValid) {
+        throw new Error('token is not valid')
+      }
+      res.send();
+    })
+    .catch(() => {
+      res.status(403).send()
+    })
+}
 
 const postDeck = (req, res) => {
   saveDeck(req.body, res);
 };
 
 const getOneDeck = (req, res) => {
-  console.log(req.query.name);
   findOneDeck(req.query.name, res);
 };
 
@@ -28,11 +50,70 @@ const handleDeleteDeck = (req, res) => {
   deleteDeck(req.body.name, res);
 };
 
+// TODO: refactor this into seperate file later
+const signupHandler = (req, res) => {
+  let email = req.body.email;
+  let username = req.body.username;
+  let [hash, salt] = util.createHash(req.body.password);
+  // reject empty username, pw, email fields
+  if (!email || !username) {
+    res.status(400).send();
+  } else {
+    User.findOne({ email })
+      .exec()
+      .then(result => {
+        if (result) {
+          throw result;
+        }
+        let newUser = new User({ email, username, hash, salt });
+        return newUser.save();
+      })
+      .then(results => {
+        return Session.findOneAndUpdate({ hash: req.session.hash }, { userId: results._id })
+          .exec()
+          .then(() => {
+            res.status(201).send(results.username);
+          })
+      })
+      .catch(user => {
+        let error = new Error('An account already exists for this user');
+        res.status(400).send(error);
+      });
+  }
+};
+
+// TODO: actually do something after comparing passwords
+const loginHandler = (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    res.status(400).send();
+  } else {
+    User.findOne({ username })
+      .exec()
+      .then(user => {
+        if (!user || !user.comparePassword(password)) {
+          throw user;
+        } 
+        return Session.findOneAndUpdate({ hash: req.session.hash }, { userId: user._id })
+          .exec()
+      })
+      .then(() => {
+        res.status(201).send();
+      })
+      .catch(user => {
+        res.status(503).send();
+      })
+  }
+}
+
 module.exports = {
+  isLoggedIn,
   postDeck,
   getOneDeck,
   getAllDecks,
   updateWithNewCards,
   handleDeleteDeck,
-  updateDeckProps
+  updateDeckProps,
+  signupHandler,
+  loginHandler
 };
